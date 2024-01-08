d6ce47b9cb5ab98501426221b055530596e5e332
Navadhesh <133025163+Navadhesh@users.noreply.github.com>
2024-01-08 14:27:53 +0530

    Add files via upload

---
+++ src/TablesJavaCode.java
@@ -0,0 +1,12 @@
+public class TablesJavaCode
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        System.out.println(n+" Tables");
+        for(int i=1;i<=10;i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+}
